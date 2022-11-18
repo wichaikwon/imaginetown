4962d208da5c696e6228279536e0bb6502063378
Cl2am3l <57160434@go.buu.ac.th>
2022-11-18 19:56:31 +0700

    update margin

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -5,4 +5,5 @@ export default function handler(req, res) {
   const _id = req.query.id
   const merged = [...data['NOW_SHOWING'], ...data['COMING_SOON']]
   res.status(200).json(merged.filter(({ id }) => id === _id))
+  
 }
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,50 @@
-import AppBar from '@mui/material/AppBar'
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-import Stack from '@mui/material/Stack'
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import useAxios from 'axios-hooks'
-import styled from '@mui/system/styled';
-import Chip from '@mui/material/Chip';
-import { useState } from 'react';
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import useAxios from "axios-hooks";
+import styled from "@mui/system/styled";
+import Chip from "@mui/material/Chip";
+import { useState } from "react";
 
 const MovieTitle = styled(Typography)({
-  color: '#F1AD3F',
+  color: "#F1AD3F",
   fontWeight: 700,
   fontSize: 14,
-})
+});
 const Dresciption = styled(Typography)({
-  color: '#FFFFF',
+  color: "#FFFFFF",
   fontWeight: 700,
   fontSize: 18,
-})
-
-
+});
 
 export default function Home() {
-
   const [state, setState] = useState("NOW_SHOWING");
 
   const [{ data, loading, error }, refetch] = useAxios(
-    'http://localhost:3000/api/'
-  )
-  if (loading) return <p>loding...</p>
-  if (error) return <p>Error...</p>
-
-
-
+    "http://localhost:3000/api/"
+  );
+  if (loading) return <p>loding...</p>;
+  if (error) return <p>Error...</p>;
 
   return (
-    <Box height="200vh"
+    <Box
+      height="200vh"
       sx={{
-        background: "linear-gradient(123.95deg, rgba(0, 0, 0, 0.9) 12.92%, rgba(131, 0, 0, 0.9) 87%)"
-      }}>
-      <AppBar position='sticky'>
-        <Stack direction="row" gap={1} alignItems="center" justifyContent="space-between">
+        background:
+          "linear-gradient(123.95deg, rgba(0, 0, 0, 0.9) 12.92%, rgba(131, 0, 0, 0.9) 87%)",
+      }}
+    >
+      <AppBar position="sticky">
+        <Stack
+          direction="row"
+          gap={1}
+          alignItems="center"
+          justifyContent="space-between"
+        >
           <Toolbar>test</Toolbar>
           <Toolbar>
             <Typography
@@ -52,93 +54,110 @@ export default function Home() {
               href="/"
               sx={{
                 mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
                 fontWeight: 700,
-                letterSpacing: '.1rem',
-                color: 'inherit',
-                textDecoration: 'none',
+                letterSpacing: ".1rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
             >
               imagineTown
             </Typography>
           </Toolbar>
-          <Toolbar>Cart <a>&nbsp;Login</a></Toolbar>
+          <Toolbar>
+            Cart <a>&nbsp;Login</a>
+          </Toolbar>
         </Stack>
       </AppBar>
-      <Container
-        sx={{
-          color: 'white'
-        }}>
-        <Box
-        sx={{
-          backgroundColor : '#FFFFFF1A'
-        }}
+      <Container>
+        <Stack p={3}
+          sx={{
+            backgroundColor: "#FFFFFF1A",
+          }}
         >
-          <Stack>
-            <Stack direction="row" gap={1} justifyContent="center">
-              <Button
-                onClick={() => setState("NOW_SHOWING")}
-                sx={{
-                  color: state === "NOW_SHOWING" ? "#FFFF" : "#FFFFFF50",
-                  fontWeight: 700,
-                  fontSize: 20,
-                  textTransform: 'none'
-                }}>
-                Now Showing
-              </Button>
-              <Button
-                onClick={() => setState("COMING_SOON")}
-                sx={{
-                  // color: t => t.palette.common.white,
-                  color: state === "COMING_SOON" ? "#FFFF" : "#FFFFFF50",
-                  fontWeight: 700,
-                  fontSize: 20,
-                  textTransform: 'none'
-                }} >
-                Coming Soon
-              </Button>
-            </Stack>
+          <Stack direction="row" gap={1} justifyContent="center">
+            <Button
+              onClick={() => setState("NOW_SHOWING")}
+              sx={{
+                color: state === "NOW_SHOWING" ? "#FFFF" : "#FFFFFF50",
+                fontWeight: 700,
+                fontSize: 20,
+                textTransform: "none",
+              }}
+            >
+              Now Showing
+            </Button>
+            <Button
+              onClick={() => setState("COMING_SOON")}
+              sx={{
+                // color: t => t.palette.common.white,
+                color: state === "COMING_SOON" ? "#FFFF" : "#FFFFFF50",
+                fontWeight: 700,
+                fontSize: 20,
+                textTransform: "none",
+              }}
+            >
+              Coming Soon
+            </Button>
           </Stack>
           <Box>
-            <Stack direction="row" gap={1} flexWrap="wrap">
-              {data[state].map(idx => {
+            <Stack
+              direction="row"
+              gap={4}
+              justifyContent="center"
+              flexWrap="wrap"
+            >
+              {data[state].map((idx) => {
                 return (
-                  <Stack key={idx.id}>
-                    <img src={idx.image}
-                      style=
-                      {{
-                        width: 250,
-                        height: 300,
-                        borderRadius: 10
-                      }}
-
-                    />
-                    <MovieTitle>{idx.date}</MovieTitle>
-                    <Dresciption>{idx.name.en}</Dresciption>
-                    <Dresciption>{idx.name.th}</Dresciption>
+                  <Stack 
+                  width={250} 
+                  key={idx.id}>
+                    <Stack>
+                      <img
+                        src={idx.image}
+                        style={{
+                          width: 250,
+                          height: 300,
+                          borderRadius: 10,
+                        }}
+                      />
+                    </Stack>
+                    <Stack>
+                      <MovieTitle>{idx.date}</MovieTitle>
+                    </Stack>
+                    <Stack>
+                      <Dresciption>{idx.name.en}</Dresciption>
+                    </Stack>
+                    <Stack>
+                      <Dresciption>{idx.name.th}</Dresciption>
+                    </Stack>
                     <Stack direction="row" gap={1}>
                       <Chip
                         sx={{
-                          background: '#E1E1E7',
+                          background: "#E1E1E7",
                           borderRadius: 10,
-                          color: '#838388',
-                        }} label={idx.type} />
+                          color: "#838388",
+                        }}
+                        label={idx.type}
+                      />
                       <Chip
                         sx={{
-                          background: '#E1E1E7',
+                          background: "#E1E1E7",
                           borderRadius: 10,
-                          color: '#838388',
-                        }} label={idx.duration} />
+                          color: "#838388",
+                        }}
+                        label={idx.duration}
+                      />
                     </Stack>
                   </Stack>
-                )
+                );
               })}
               <Stack></Stack>
             </Stack>
           </Box>
-        </Box>
+        </Stack>
       </Container>
     </Box>
-  )
+  );
 }
